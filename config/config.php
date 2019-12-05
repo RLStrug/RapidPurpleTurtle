af78f487726824469b6450815e2cb20f2bbc446b
Cyril Govin <cgovin@emi.u-bordeaux1.fr>
2019-12-05 23:38:41 +0100

    Ajout config bdd - TO COMPLETE

---
+++ config/config.php
@@ -0,0 +1,27 @@
+<?php
+cass Conf {
+    static private $debug = true;
+	
+    static private $databases = array(
+        'hostname' => 'localhost',
+        'database_name' => 'ChiCI',
+        'login' => 'root',
+        'password' => ''
+    );
+    static public function getDebug() {
+        return self::$debug;
+    }
+    static public function getLogin() {
+        return self::$databases['login'];
+    }
+    static public function getHostName() {
+        return self::$databases['hostname'];
+    }
+    static public function getDatabaseName() {
+        return self::$databases['database_name'];
+    }
+    static public function getPassword() {
+        return self::$databases['password'];
+    }
+}
+?>
