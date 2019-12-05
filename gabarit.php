796979ff08137df0ff5057e3fa322bd01adcd8b3
Lucas <lucas.perez@etu.u-bordeaux.fr>
2019-12-05 22:32:09 +0100

    add gabarit.php

---
+++ gabarit.php
@@ -0,0 +1,57 @@
+<!doctype html>
+
+<?php
+  if (!isset($_SESSION["language"])){
+    $_SESSION["language"] = "fr";
+  }
+  if (!isset($_SESSION["encoding"])){
+    $_SESSION["encoding"] = "UTF-8";
+  }
+?>
+
+<html lang=<?=$_SESSION["language"]?>>
+
+  <head>
+    <meta charset=<?=$_SESSION["encoding"]?>>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <?=$includes?>
+    <title> <?=$title?> </title>
+  </head>
+
+
+  <body>
+    <style>
+    .navbar {
+        font-size: 17pt
+    }
+    </style>
+    <title>Page</title>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark" >
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
+        <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Link</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Link</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+
+    <main>
+      <?=$content?>
+    </main>
+
+
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+  </body>
+</html>
