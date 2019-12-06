51b6f6d05d971d5ddf265f4949d1a481885537e3
Kilian Roux <kroux@emi.u-bordeaux1.fr>
2019-12-06 01:32:00 +0100

    Droit et Soutien

--- Droit_Soutien.php
+++ Droit_Soutien.php
@@ -15,14 +15,13 @@
 
 <h1> Droit et soutien </h1>
 <ul class="list-group">
-  <li class="list-group-item active"><img src="./resources/gray_money.png" width=32 height="32" title="Bootstrap">  -  Aide financiéres</li>
-  <li class="list-group-item"><img src="./resources/gray_money.png" width=32 height="32" title="Bootstrap">  -  Aide financiéres</li>
-  <li class="list-group-item"><img src="./resources/gray_money.png" width=32 height="32" title="Bootstrap">  -  Aide financiéres</li>
-  <li class="list-group-item"><img src="./resources/gray_money.png" width=32 height="32" title="Bootstrap">  -  Aide financiéres</li>
-  <li class="list-group-item"><img src="./resources/gray_money.png" width=32 height="32" title="Bootstrap">  -  Aide financiéres</li>
+  <li class="list-group-item"><img src="./resources/money.png" width=32 height="32" title="Bootstrap">  -  Aide financiéres</li>
+  <li class="list-group-item"><img src="./resources/house.png" width=32 height="32" title="Bootstrap">  -  Aide au logement</li>
+  <li class="list-group-item"><img src="./resources/help.png" width=32 height="32" title="Bootstrap">  -  Se soigner à moindre coût</li>
+  <li class="list-group-item"><img src="./resources/gray_money.png" width=32 height="32" title="Bootstrap">  -  Conseils juridiques</li>
+  <li class="list-group-item"><img src="./resources/earth.png" width=32 height="32" title="Bootstrap">  -  Migrant/Réfugiés/Demandeur d'asile</li>
 </ul>
 
-<h2> </h2>
 
 <?php $content=ob_get_clean(); ?>
 
